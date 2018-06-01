6a8fdc6f6a458af971e3344e04a578595381fea5
Jair Pacheco <pachecograu@gmail.com>
2018-05-31 21:21:45 -0500

    init

---
+++ www/js/controllers/loginCtrl.js
@@ -0,0 +1,3 @@
+angular.module('pulos').controller('longinCtrl', ['$scope', function($scope) {
+  console.log('en el login');
+}]);
---
+++ www/js/controllers/profileCtrl.js
@@ -0,0 +1,3 @@
+angular.module('pulos').controller('profileCtrl', ['$scope', function($scope) {
+  console.log('en el profile');
+}]);
